ec1778d0f60ec702149fd81fb7eabb40a7573533
Laurence Lord <laurence.lord@gmail.com>
2023-01-26 20:20:48 +0000

    Draft skeleton CSS selectors

--- src/components/ItemSelector.tsx
+++ src/components/ItemSelector.tsx
@@ -1,25 +1,26 @@
 import { Source_Sans_Pro } from "@next/font/google";
+import styles from "@/styles/ItemSelector.module.css";
 
 const sans = Source_Sans_Pro({ subsets: ["latin"], weight: "400" });
 
 export const ItemSelector = () => (
-  <header className={`${sans.className} itemSelector`}>
-    <div className="itemSummary">
-      <div className="itemIconSlot"></div>
-      <h1 className="itemName">Pickle</h1>
-      <p className="itemStock">
-        <span className="currentStock">60kg</span>/
-        <span className="maximumStock">150kg</span>
+  <header className={`${sans.className} ${styles.itemSelector}`}>
+    <div className={styles.itemSummary}>
+      <div className={styles.itemIconSlot}></div>
+      <h1 className={styles.itemName}>Pickle</h1>
+      <p className={styles.itemStock}>
+        <span className={styles.currentStock}>60kg</span>/
+        <span className={styles.maximumStock}>150kg</span>
       </p>
     </div>
-    <div className="itemSelect">
-      <i className="itemMenuIcon"></i>
-      <span className="itemMenuText">
-        <span className="itemMenuName">Pickle</span> |{" "}
-        <span className="itemMenuId">#1234</span>
+    <div className={styles.itemSelect}>
+      <i className={styles.itemMenuIcon}></i>
+      <span className={styles.itemMenuText}>
+        <span className={styles.itemMenuName}>Pickle</span> |{" "}
+        <span className={styles.itemMenuId}>#1234</span>
       </span>
-      <button className="itemMenuDropdown">
-        <i className="itemMenuDropdownDecoration"></i>
+      <button className={styles.itemMenuDropdown}>
+        <i className={styles.itemMenuDropdownDecoration}></i>
       </button>
     </div>
   </header>
