5db0714fbc9a14e8a707b6ee46ff16589da98516
Laurence Lord <laurence.lord@gmail.com>
2023-01-26 20:38:02 +0000

    Apply CSS selectors to everything

--- src/components/ChartBody.tsx
+++ src/components/ChartBody.tsx
@@ -5,13 +5,13 @@ const sans = Source_Sans_Pro({ subsets: ["latin"], weight: "400" });
 
 export const ChartBody = () => (
   <div className={styles.chartHolder}>
-    <svg className="chartLine actual"></svg>
-    <svg className="chartLine projected"></svg>
-    <svg className="chartLine demand"></svg>
-    <table className="chartTable">
+    <svg className={styles.chartLine + " " + styles.actual}></svg>
+    <svg className={styles.chartLine + " " + styles.projected}></svg>
+    <svg className={styles.chartLine + " " + styles.demand}></svg>
+    <table className={styles.table}>
       <caption className="screenReaderOnly">Stock Level for pickles</caption>
-      <thead>
-        <tr>
+      <thead className={styles.tHead}>
+        <tr className={styles.headRow}>
           <th>Stock Type</th>
           <th>Date</th>
           <th>Stocks (kg)</th>
@@ -19,53 +19,53 @@ export const ChartBody = () => (
           <th>Day</th>
         </tr>
       </thead>
-      <tbody>
-        <tr>
-          <td>Actual</td>
-          <td>SEP 5th, 2022</td>
-          <td>70kg</td>
-          <td>30kg</td>
-          <td>Tue</td>
+      <tbody className={styles.tBody}>
+        <tr className={styles.tBar}>
+          <td className={styles.valueType}>Actual</td>
+          <td className={styles.dateValue}>SEP 5th, 2022</td>
+          <td className={styles.stockValue}>70kg</td>
+          <td className={styles.demandValue}>30kg</td>
+          <td className={styles.barLabel}>Tue</td>
         </tr>
-        <tr>
-          <td>Today</td>
-          <td>SEP 6rd, 2022</td>
-          <td>60kg</td>
-          <td>20kg</td>
-          <td>Wed</td>
+        <tr className={styles.tBar}>
+          <td className={styles.valueType}>Today</td>
+          <td className={styles.dateValue}>SEP 6rd, 2022</td>
+          <td className={styles.stockValue}>60kg</td>
+          <td className={styles.demandValue}>20kg</td>
+          <td className={styles.barLabel}>Wed</td>
         </tr>
-        <tr>
-          <td>Projected</td>
-          <td>SEP 7th, 2022</td>
-          <td>50kg</td>
-          <td>30kg</td>
-          <td>Thu</td>
+        <tr className={styles.tBar}>
+          <td className={styles.valueType}>Projected</td>
+          <td className={styles.dateValue}>SEP 7th, 2022</td>
+          <td className={styles.stockValue}>50kg</td>
+          <td className={styles.demandValue}>30kg</td>
+          <td className={styles.barLabel}>Thu</td>
         </tr>
       </tbody>
     </table>
-    <div className="screenOnly yAxis">
-      <p className="axisLabel">kg</p>
-      <ul className="axisUnits">
-        <li className="axisUnit">
-          <span className="unitText">100</span>
-          <i className="unitGuide"></i>
+    <div className={"screenOnly " + styles.yAxis}>
+      <p className={styles.yAxisLabel}>kg</p>
+      <ul className={styles.yAxisUnits}>
+        <li className={styles.yAxisUnit}>
+          <span className={styles.unitText}>100</span>
+          <i className={styles.unitGuide}></i>
         </li>
-        <li className="axisUnit">
-          <span className="unitText">75</span>
-          <i className="unitGuide"></i>
+        <li className={styles.yAxisUnit}>
+          <span className={styles.unitText}>75</span>
+          <i className={styles.unitGuide}></i>
         </li>
-        <li className="axisUnit">
-          <span className="unitText">50</span>
-          <i className="unitGuide"></i>
+        <li className={styles.yAxisUnit}>
+          <span className={styles.unitText}>50</span>
+          <i className={styles.unitGuide}></i>
         </li>
-        <li className="axisUnit">
-          <span className="unitText">25</span>
-          <i className="unitGuide"></i>
+        <li className={styles.yAxisUnit}>
+          <span className={styles.unitText}>25</span>
+          <i className={styles.unitGuide}></i>
         </li>
       </ul>
     </div>
-    <div className="screenOnly xAxis">
-      <p className="axisLabel">Days</p>
+    <div className={"screenOnly " + styles.xAxis}>
+      <p className={styles.axisLabel}>Days</p>
     </div>
   </div>
 );
