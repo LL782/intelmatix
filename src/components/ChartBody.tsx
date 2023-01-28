544b1d59962b190395f88afdec7eb84c8d33db41
Laurence Lord <laurence.lord@gmail.com>
2023-01-28 13:54:00 +0000

    Move chart variables into JS scope

--- src/components/ChartBody.tsx
+++ src/components/ChartBody.tsx
@@ -1,16 +1,31 @@
+import { CSSProperties } from "react";
 import styles from "@/styles/ChartBody.module.css";
 import { ChartYAxis } from "./ChartYAxis";
 import { ChartBars } from "./ChartBars";
 import { ChartLines } from "./ChartLines";
 import { ChartXAxisLegend } from "./ChartXAxisLegend";
 
-export const ChartBody = () => (
-  <div className={styles.chartBody}>
-    <ChartYAxis />
-    <div className={styles.chartArea}>
-      <ChartBars />
-      <ChartLines />
+export const ChartBody = () => {
+  const widthToHeightRatio = 1;
+  const numHorizontalGuides = 5;
+  const numChartBars = 3;
+  const gutterPercent = 0.0075;
+
+  const customStyle = {
+    "--widthToHeightRatio": widthToHeightRatio,
+    "--numHorizontalGuides": numHorizontalGuides,
+    "--numChartBars": numChartBars,
+    "--gutterPercent": gutterPercent,
+  } as CSSProperties;
+
+  return (
+    <div className={styles.chartBody} style={customStyle}>
+      <ChartYAxis />
+      <div className={styles.chartArea}>
+        <ChartBars />
+        <ChartLines />
+      </div>
+      <ChartXAxisLegend />
     </div>
-    <ChartXAxisLegend />
-  </div>
-);
+  );
+};
