2aaf9ca86654ba4740dda69a0a6cfdea01638e80
Laurence Lord <laurence.lord@gmail.com>
2023-01-29 18:02:21 +0000

    Restructure useChartVariables

--- src/hooks/useChartVariables.test.tsx
+++ src/hooks/useChartVariables.test.tsx
@@ -4,37 +4,32 @@ import useChartVariables from "./useChartVariables";
 const [DAY_0, DAY_1, DAY_2] = stockData.days;
 
 describe("useChartVariables", () => {
-  describe("Given three days of data", () => {
+  describe("shares general layout variables:", () => {
     const result = useChartVariables([DAY_0, DAY_1, DAY_2]);
 
-    test("shares the number of x-axis bars (3)", () => {
-      const { numXAxisBars } = result;
-      expect(numXAxisBars).toBe(3);
+    test("the width-to-height ratio of the chart", () => {
+      const { widthToHeightRatio } = result;
+      expect(widthToHeightRatio).toBeDefined();
+    });
+    test("the gutter width between x-axis bars as a percentage", () => {
+      const { xAxisGutterPercentage } = result;
+      expect(xAxisGutterPercentage).toBeDefined();
     });
 
-    describe("shares general layout variables:", () => {
-      test("the width-to-height ratio of the chart", () => {
-        const { widthToHeightRatio } = result;
-        expect(widthToHeightRatio).toBeDefined();
-      });
-      test("the gutter width between x-axis bars as a percentage", () => {
-        const { xAxisGutterPercentage } = result;
-        expect(xAxisGutterPercentage).toBeDefined();
-      });
-
-      test("the number of y-axis guides", () => {
-        const { numYAxisGuides } = result;
-        expect(numYAxisGuides).toBeDefined();
-      });
+    test("the number of y-axis guides", () => {
+      const { numYAxisGuides } = result;
+      expect(numYAxisGuides).toBeDefined();
     });
   });
 
-  describe("Given two days of data", () => {
-    const result = useChartVariables([DAY_0, DAY_1]);
-
-    test("shares the number of x-axis bars (2)", () => {
-      const { numXAxisBars } = result;
-      expect(numXAxisBars).toBe(2);
+  describe("generates variables based on the data passes in", () => {
+    test.each([
+      [1, [DAY_0], 1],
+      [2, [DAY_0, DAY_1], 2],
+      [3, [DAY_0, DAY_1, DAY_2], 3],
+    ])("shares the number of x-axis bars (%d)", (expected, input) => {
+      const { numXAxisBars } = useChartVariables(input);
+      expect(numXAxisBars).toBe(expected);
     });
   });
 });
