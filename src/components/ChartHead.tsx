13a5814a7e559eb069bfa0f53020782879a2991e
Laurence Lord <laurence.lord@gmail.com>
2023-01-30 17:24:32 +0000

    Position chart heading

--- src/components/ChartHead.tsx
+++ src/components/ChartHead.tsx
@@ -1,10 +1,8 @@
 import styles from "@/styles/ChartHead.module.css";
 
 export const ChartHead = () => (
-  <header className={styles.chartHeader}>
-    <div className={styles.titleSlot}>
-      <h2 className={styles.stockLevelTitle}>Stock Level</h2>
-    </div>
+  <header className={`${styles.chartHeader}`}>
+    <h2 className={styles.stockLevelTitle}>Stock Level</h2>
     <div className={styles.descriptionSlot}>
       <h3 className={styles.chip}>Live Marketing Campaign</h3>
       <ul className={styles.chartKey}>
