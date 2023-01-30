6c9baab7da3e001a73d652cbf98f140b31145137
Laurence Lord <laurence.lord@gmail.com>
2023-01-30 12:21:45 +0000

    Return a 404 for unexpected paths

--- src/pages/stock-level/[item].tsx
+++ src/pages/stock-level/[item].tsx
@@ -1,14 +1,13 @@
 import { StockData } from "@/types/StockData";
 import { StockPage } from "../../components/StockPage";
 import { EXPECTED_ITEMS } from "@/config/expectedItems";
+import Error from "next/error";
 
 interface Props {
   data: StockData;
 }
 
 const Page = ({ data }: Props) => <StockPage data={data} />;
-
-export default Page;
 
 export async function getStaticPaths() {
   return {
@@ -19,12 +18,15 @@ export async function getStaticPaths() {
 
 export async function getStaticProps({ params }: any) {
   const { item } = params;
+
+  if (EXPECTED_ITEMS.includes(item) === false) {
+    return <Error statusCode={404} />;
+  }
+
   const res = await fetch(`${process.env.API_ROUTES_URL}/stock/${item}`);
   const data = await res.json();
 
-  return {
-    props: {
-      data,
-    },
-  };
+  return { props: { data } };
 }
+
+export default Page;
