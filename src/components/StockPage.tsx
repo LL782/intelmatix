be4e07434249870e4b685b549057161ab71c6516
Laurence Lord <laurence.lord@gmail.com>
2023-01-30 04:36:01 +0000

    Extract StockPage from example

---
+++ src/components/StockPage.tsx
@@ -0,0 +1,20 @@
+import Head from "next/head";
+import { ItemBar } from "@/components/ItemBar";
+import { Chart } from "@/components/Chart";
+import { Modal } from "@/components/Modal";
+import { StockData } from "@/types/StockData";
+
+export const StockPage = ({ data }: { data: StockData }) => (
+  <>
+    <Head>
+      <title>Stock Control - {data.itemName} - Intelmatix x LL782</title>
+      <meta name="description" content="Example application" />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <Modal>
+      <ItemBar data={data} />
+      <Chart data={data} />
+    </Modal>
+  </>
+);
--- src/pages/stock-level/example.tsx
+++ src/pages/stock-level/example.tsx
@@ -1,23 +1,6 @@
-import Head from "next/head";
-
-import { ItemBar } from "@/components/ItemBar";
-import { Chart } from "@/components/Chart";
-import { Modal } from "@/components/Modal";
 import { example as data } from "@/examples/stockData";
+import { StockPage } from "../../components/StockPage";
 
-const ExampleStockPage = () => (
-  <>
-    <Head>
-      <title>Stock Control - {data.itemName} - Intelmatix x LL782</title>
-      <meta name="description" content="Example application" />
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-    <Modal>
-      <ItemBar data={data} />
-      <Chart data={data} />
-    </Modal>
-  </>
-);
+const ExampleStockPage = () => <StockPage data={data} />;
 
 export default ExampleStockPage;
