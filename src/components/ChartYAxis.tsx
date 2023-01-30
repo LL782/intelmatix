2c1339ce75f2fa15e82bc7d5d7e0e73f6d2f3bcf
Laurence Lord <laurence.lord@gmail.com>
2023-01-30 00:54:44 +0000

    Flow data into ChartYAxis

--- src/components/ChartYAxis.tsx
+++ src/components/ChartYAxis.tsx
@@ -1,33 +1,25 @@
+import useChartVariables from "@/hooks/useChartVariables";
 import styles from "@/styles/ChartYAxis.module.css";
 
 interface Props {
   unitOfMeasurement: string;
+  yAxisGuides: ReturnType<typeof useChartVariables>["yAxisGuides"];
 }
 
-export const ChartYAxis = ({ unitOfMeasurement }: Props) => (
-  <div className={styles.yAxis}>
-    <p className={styles.yAxisLegend}>{unitOfMeasurement}</p>
-    <ul className={styles.yAxisGuides}>
-      <li className={styles.yAxisGuide}>
-        <span className={styles.yAxisGuideLabel}>100</span>
-        <i className={styles.yAxisGuideline}></i>
-      </li>
-      <li className={styles.yAxisGuide}>
-        <span className={styles.yAxisGuideLabel}>75</span>
-        <i className={styles.yAxisGuideline}></i>
-      </li>
-      <li className={styles.yAxisGuide}>
-        <span className={styles.yAxisGuideLabel}>50</span>
-        <i className={styles.yAxisGuideline}></i>
-      </li>
-      <li className={styles.yAxisGuide}>
-        <span className={styles.yAxisGuideLabel}>25</span>
-        <i className={styles.yAxisGuideline}></i>
-      </li>
-      <li className={styles.yAxisGuide}>
-        <span className={styles.yAxisGuideLabel}>0</span>
-        <i className={styles.yAxisGuideline}></i>
-      </li>
-    </ul>
-  </div>
-);
+export const ChartYAxis = ({ unitOfMeasurement, yAxisGuides }: Props) => {
+  const guides = [...yAxisGuides].reverse();
+
+  return (
+    <div className={styles.yAxis}>
+      <p className={styles.yAxisLegend}>{unitOfMeasurement}</p>
+      <ul className={styles.yAxisGuides}>
+        {guides.map((number) => (
+          <li className={styles.yAxisGuide} key={number}>
+            <span className={styles.yAxisGuideLabel}>{number}</span>
+            <i className={styles.yAxisGuideline}></i>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
